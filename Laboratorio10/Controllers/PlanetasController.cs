2be78785b9c82504744bc39c27cda345cd0f5dcc
Juan-Ignacio-Pacheco-Castro <juan.pachecocastro@ucr.ac.cr>
2021-11-23 10:50:29 -0600

    Added solution of lab 4

---
+++ Laboratorio10/Controllers/PlanetasController.cs
@@ -0,0 +1,100 @@
+﻿using Laboratorio4.Models;
+using Laboratorio4.Handlers;
+using System.Web.Mvc;
+using System;
+
+namespace Laboratorio4.Controllers
+{
+    public class PlanetasController : Controller
+    {
+        public ActionResult listadoDePlanetas()
+        {
+            PlanetasHandler accesoDatos = new PlanetasHandler();
+            ViewBag.planetas = accesoDatos.obtenerTodosLosPlanetas();
+            return View();
+        }
+
+        public ActionResult crearPlaneta()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult crearPlaneta(PlanetaModel planeta)
+        {
+            ViewBag.ExitoAlCrear = false;
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    PlanetasHandler accesoDatos = new PlanetasHandler();
+                    ViewBag.ExitoAlCrear = accesoDatos.crearPlaneta(planeta);
+                    if (ViewBag.ExitoAlCrear)
+                    {
+                        ViewBag.Message = "El planeta " + planeta.nombre + " fue creado con éxito :)";
+                        ModelState.Clear();
+                    }
+                }
+                return View();
+            }
+            catch
+            {
+                ViewBag.Message = "Algo salió mal y no fue posible crear el planeta :(";
+                return View();
+            }
+        }
+
+        [HttpGet]
+        public ActionResult editarPlaneta(int ? identificador)
+        {
+            ActionResult vista;
+            try
+            {
+                PlanetasHandler accesoDatos = new PlanetasHandler();
+                PlanetaModel planetaModificar = accesoDatos.obtenerTodosLosPlanetas().Find(smodel => smodel.id == identificador);
+                if (planetaModificar == null)
+                {
+                    vista = RedirectToAction("listadoDePlanetas");
+                }
+                else
+                {
+                    vista = View(planetaModificar);
+                }
+            }
+            catch
+            {
+                vista = RedirectToAction("listadoDePlanetas");
+            }
+            return vista;
+        }
+
+        [HttpPost]
+        public ActionResult editarPlaneta(PlanetaModel planeta)
+        {
+            try
+            {
+                PlanetasHandler accesoDatos = new PlanetasHandler();
+                accesoDatos.modificarPlaneta(planeta);
+                return RedirectToAction("listadoDePlanetas");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        [HttpGet]
+        public FileResult accederArchivo(int identificador)
+        {
+            try
+            {
+                PlanetasHandler accesoDatos = new PlanetasHandler();
+                var tupla = accesoDatos.descargarContenido(identificador);
+                return File(tupla.Item1, tupla.Item2);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
