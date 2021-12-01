575ab718cd907f966cec5b75ee7ffb2c6904688a
Juan-Ignacio-Pacheco-Castro <juan.pachecocastro@ucr.ac.cr>
2021-12-01 15:46:52 -0600

    Lab11 is not finished yet, gonna try exploring UI testing

---
+++ Laboratorio10.Tests/MoqExamples/Example1.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace WebApplication2.Tests.MoqExamples {
+    public interface NamesService {
+        string GetName();
+    }
+
+    [TestClass]
+    public class Example1 {
+        [TestMethod]
+        public void TestGetNamesFromNamesService() {
+            var mock = new Mock<NamesService>();
+            mock.Setup(mocked => mocked.GetName()).Returns("SuNombre");
+            Assert.AreEqual("SuNombre", mock.Object.GetName());
+        }
+    }
+}
---
+++ Laboratorio10.Tests/MoqExamples/Example2.cs
@@ -0,0 +1,43 @@
+﻿using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace WebApplication2.Tests.MoqExamples {
+    public interface IDogsUpdate {
+        bool Update(string breed);
+    }
+    
+    public class HuskyController : Controller {
+        protected IDogsUpdate updateDogsService;
+        public HuskyController(IDogsUpdate updateDogsService) {
+            this.updateDogsService = updateDogsService;
+        }
+        public ActionResult UpdateDog(string breed) {
+            var result = this.updateDogsService.Update(breed);
+            //Add some logic here
+            if (breed == "husky") {
+                ViewBag.Message = "The breed is Husky";
+                return View("UpdateDogHusky");
+            }
+            return null;
+        }
+    }
+
+    [TestClass]
+    public class Example2 {
+        [TestMethod]
+        public void TestUpdateDogFromDogsController() {
+            //Act
+            var huskyServiceMock = new Mock<IDogsUpdate>();
+            huskyServiceMock.Setup(huskyService => huskyService.Update("husky")).Returns(true);
+
+            //Arrange
+            var huskyController = new HuskyController(huskyServiceMock.Object);
+            ViewResult vista = huskyController.UpdateDog("husky") as ViewResult;
+
+            //Assert
+            Assert.IsNotNull(vista);
+            Assert.AreEqual("The breed is Husky", vista.ViewBag.Message);
+        }
+    }
+}
